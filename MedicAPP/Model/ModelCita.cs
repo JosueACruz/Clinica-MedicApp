5515bbfbd2e11225e0c58edecd095bc15e210e2e
Josue Cruz <josuecruz@codepsadecv.com>
2021-09-26 00:24:29 -0600

    Modelo de cita ya se crearon las funciones de ver, validar, insertar y buscar

---
+++ MedicAPP/Model/ModelCita.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicAPP.Model
+{
+    class ModelCita
+    {
+        public SqlDataReader cargarCita()
+        {
+            Conexion cnx = new Conexion();
+            string Consulta = "SELECT ci.idCita, ci.idPaciente, (pa. Nombre + ' '+ pa.Apellido) as 'Paciente', ci.Fecha, ci.Hora FROM Cita ci INNER JOIN Paciente pa ON ci.idPaciente = pa.idPaciente;";
+            SqlConnection con = cnx.Conectar();
+            SqlCommand cmd = new SqlCommand(Consulta, con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            return sdr;
+        }
+
+        public bool insertarCita(int paciente, DateTime fecha, string hora)
+        {
+            try
+            {
+                Conexion cnx = new Conexion();
+                string consulta = "INSERT INTO Cita values ("+paciente+", '"+fecha+"', '"+hora+"')";
+                DataSet ds = cnx.Conx(consulta);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar la cita" + ex.Message, "MedicAPP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        //Validando para saber si ya existe la categoria a ingresar
+        public bool validar(DateTime fecha, string hora)
+        {
+            try
+            {
+                Conexion cnx = new Conexion();
+                string consulta = "SELECT * FROM Cita where fecha = '"+fecha+"' and Hora = '"+hora+"'";
+                DataSet ds = cnx.Conx(consulta);
+                if (ds.Tables[0].Rows.Count != 0)
+                {
+                    //ya existe
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al vaildar la cita " + ex.Message, "MedicAPP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public bool actualizarCita(int id, string categoria)
+        {
+            try
+            {
+                Conexion cnx = new Conexion();
+                string consulta = "update MedicamentoCategoria set Nombre = '" + categoria + "' where idCategoria = " + id;
+                DataSet ds = cnx.Conx(consulta);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al editar la categoria" + ex.Message, "MedicAPP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public bool eliminarCategoriaMedicamento(int id)
+        {
+            try
+            {
+                Conexion cnx = new Conexion();
+                string consulta = "delete from MedicamentoCategoria where idCategoria = '" + id + "'";
+                DataSet ds = cnx.Conx(consulta);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar la categoria" + ex.Message, "MedicAPP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public SqlDataReader buscarCategoriaMedicamento(string paciente)
+        {
+            Conexion cnx = new Conexion();
+            string consulta = "SELECT ci.idCita, ci.idPaciente, (pa. Nombre + ' '+ pa.Apellido) as 'Paciente', ci.Fecha, ci.Hora FROM Cita ci INNER JOIN Paciente pa ON ci.idPaciente = pa.idPaciente where pa.Nombre lIKE '"+ paciente +"%' OR pa.Apellido like '"+paciente+"%'";
+            SqlConnection con = cnx.Conectar();
+            SqlCommand cmd = new SqlCommand(consulta, con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            return sdr;
+        }
+    }
+}
